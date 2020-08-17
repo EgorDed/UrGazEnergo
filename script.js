417e660be0a6469dc79c2546d5c1dd68396c6dfa
EgorDed <69810825+EgorDed@users.noreply.github.com>
2020-08-17 18:22:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function f_click(Button) {
+    if (Button.previousElementSibling.style.display === 'block') {
+        Button.previousElementSibling.style.display = 'none';
+        Button.innerHTML = 'ПОДРОБНЕЕ';
+        Button.parentNode.style.width = '25%';
+
+        Button.parentElement.style.order = 'initial';
+
+        Button.scrollIntoView({block:'center',behavior:'smooth'});
+
+        Button.style.scrollbehavior = 'smooth';
+    } else {
+        Button.previousElementSibling.style.display = 'block';
+        Button.innerHTML = 'СВЕРНУТЬ';
+        Button.parentNode.style.width = '83%';
+
+        Button.parentElement.style.order = '-1';
+
+        Button.scrollIntoView({block:'center',behavior:'smooth'});
+    }
+}
+
+
+var more_span = document.getElementById('more_projects');
+
+function more_click(Button) {
+    if(more_span.style.display === 'flex'){
+        more_span.style.display = 'none';
+        Button.innerHTML = 'ЕЩЕ';
+    } else {
+        more_span.style.display = 'flex';
+        more_span.style.paddingTop = '0';
+
+        Button.innerHTML = 'СВЕРНУТЬ';
+    }
+
+}
